fe102a48445cdfdf1298f13a5bf11abefc233cfa
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-06-04 09:43:35 +0100

    Update Damilare-Olagbende.js

--- scripts/Damilare-Olagbende.js
+++ scripts/Damilare-Olagbende.js
@@ -3,4 +3,4 @@ const name = "Damilare Richard Olagbende";
 	const email = "damilareshinaayo@gmail.com";
 	const language = "Javascript";
 
-	console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} and ${email} using ${language} for stage 2 task`)
+	console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`)
